272ff50a70c65453ed38449b3b4e2376c7f75bb0
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-09-08 16:29:14 -0400

    added route to landing page button

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
     API_ENDPOINT: 'http://localhost:3000/api',
     TOKEN_KEY: 'hogwarts-client-auth-token',
-  }
+}
+
--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -4,17 +4,21 @@ import Header from '../../components/Header/Header'
 import NavBar from '../../components/Nav/NavBar'
 
 
-export default class LandingPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <NavBar />
+const LandingPage = (props) => {
+  const handleStart = () => {
+    const {history} = props 
+    history.push('./login')
+  }
+
+  return (
+    <div>
+      <Header />
+      <NavBar />
         <p>Experience the magic of preparing for your first year of Hogwarts School of Witchcraft and Wizardry! Choose your wand, your pet, and get sorted! The journey begins...</p>
-        <button id="start">HERE</button>
-      </div>
+       <button onClick={() => handleStart()}>HERE</button>
+    </div>
     )
   }
-}
 
 
+export default LandingPage
