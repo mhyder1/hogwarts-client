ce82a69fb9848c8080e8d664f88fd2d92250730c
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-09-11 14:52:25 -0400

    deploy prep

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://arcane-forest-03071.herokuapp.com/',
+    API_ENDPOINT: 'https://arcane-forest-03071.herokuapp.com',
     TOKEN_KEY: 'hogwarts-client-auth-token',
     API_KEY: process.env.REACT_APP_API_KEY,
 }
